13a963a07063d91a414f9dc0ff0ff2baee637fb1
Spyes <lnitzberg@gmail.com>
2016-10-17 04:37:49 +0300

    CES - working quite well, still WIP

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
+/* UTIL */
+function uniqueId() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+               .toString(16)
+               .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+         s4() + '-' + s4() + s4() + s4();  
+}
+
+function log() {
+  Array.from(arguments)
+       .map(argument => console.log(argument));
+}
+
+const times = (n, fun) => (
+  Array.apply(0, Array(n))
+       .map((k, v) => fun(k, v))
+);
+
+const result = (obj = {}, path = [], defaultValue) => {
+  if (!Array.isArray(path)) return result(obj, [path]);
+  return path.reduce((curr, prev) => curr[prev], obj) || defaultValue;
+};
+
 /* STORE */
 function Store() {
   this.data = {};
+  this.subscribers = {};
   this.getStore = () => this.data;
-  this.get = (reducer, defaultValue) => this.data[reducer] || defaultValue;
+  this.get = (reducer, defaultValue) => result(this.data, reducer, defaultValue);
   this.dispatch = function (action) {
     const { type, component, entity } = action;
     switch (type) {
@@ -20,23 +47,27 @@ function Store() {
         if (!found_entity) break;
         found_entity.components.push(component);
         break;
-
+      case "CHANGE_LOCATION":
+        const ent = this.data
+			.entities
+			.find(e => e.id === entity);
+	const pos_comp = getComponent(ent, "Position");
+	pos_comp.data.location = action.location;
       default:
         break;
     }
+    if (type) {
+      result(this.subscribers, [type], []).map(fun => { fun(type, entity) });
+    }
   };
-}
-const store = new Store();
-
-/* UTIL */
-function uniqueId() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-               .toString(16)
-               .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-         s4() + '-' + s4() + s4() + s4();  
+  this.subscribe = function (actions, fun) {
+    actions.map(action => {
+      if (!Array.isArray(this.subscribers[action])) {
+	this.subscribers[action] = [];
+      }
+      this.subscribers[action].push(fun);
+    });
+  };
 }
 
 /* ENTITY */
@@ -47,6 +78,8 @@ const Entity = () => (
   }
 );
 
+/*** SIDE EFFECTS ***/
+/* ENTITY MANAGER */
 const createEntity = (store) => {
   const entity = Entity();
   const action = { type: "ENTITY_CREATED", entity };
@@ -54,23 +87,34 @@ const createEntity = (store) => {
   return entity;
 };
 
-const hasComponents = (entity, component_names) => {
-  if (component_names.length === 0) return true;  
-  const comp = component_names[0];
-  const has = entity.components.find(component => component.name === comp) !== undefined;
-  return has && hasComponents(entity, component_names.slice(1));
-};
+const getComponent = (entity, component_name) => (
+  entity.components
+	.find(component => component.name === component_name)
+);
+
+const hasComponents = (entity, component_names) => (
+  component_names.reduce((prev, curr) => (
+    prev && getComponent(entity, curr) !== undefined
+  ), true)
+);
 
 /* COMPONENT */
-const PositionComponent = () => ({
+const PositionComponent = ({location: location} = {location: ''}) => ({
   name: "Position",
-  data: { location: "" }
+  data: { location }
 });
 
-const RenderComponent = () => ({
-  name: "Render"
+const RenderComponent = ({model: model} = {model: ''}) => ({
+  name: "Render",
+  data: { model }
 });
 
+const ClickComponent = ({onClick: onClick} = {onClick: () => {}}) => ({
+  name: "Click",
+  data: { onClick }
+});
+
+/*** SIDE EFFECTS ***/
 const addComponent = (entity, component, store) => {
   const action = { type: "ADD_COMPONENT", entity, component };
   store.dispatch(action);
@@ -89,39 +133,116 @@ class System {
                .get('entities', [])
                .filter(entity => hasComponents(entity, this.componentDeps))
   }
+
+  execute() {    
+  }
+}
+
+class ClickSystem extends System {
+  constructor(props) {
+    super(props);
+
+    this.store.subscribe(["ON_CLICK"], this.subscribe);
+  }
+
+  subscribe(action, entity) {
+    switch (action) {
+      case "ON_CLICK":
+	const click_comp = getComponent(entity, "Click");
+	if (click_comp.data.onClick) click_comp.data.onClick(entity);	
+	break;
+      default: break;
+    }
+  }
 }
 
 class RenderSystem extends System {
   constructor(props) {
     super(props);
+    
+    this.render = this.render.bind(this);
+    this.subscribe = this.subscribe.bind(this);
+
+    this.store.subscribe(["CHANGE_LOCATION"], this.subscribe);
   }
 
+  subscribe(type, entity) {
+    console.log(entity);
+  }
+  
   render(entity) {
-    const position_comp = entity.components.find(comp => comp.name === "Position");
-    document.getElementById(position_comp.data.location).innerHTML = `<h4>${ entity.id }</h4>`;
+    const position_comp = getComponent(entity, "Position");
+    const render_comp = getComponent(entity, "Render");
+    const clickable_comp = getComponent(entity, "Click");
+    const { location } = position_comp.data;
+    const el = document.getElementsByClassName(location).item(0);
+    if (!el) {
+      log("Position component points to a location that doesn't exist", entity.id, position_comp);
+      return;
+    }
+    const model = result(render_comp, ['data', 'model']);
+    const className = clickable_comp.data ? 'clickable' : '';
+    if (model) el.innerHTML = `<span class='${className}'>${ model }</span>`;
+    const action = { type: "ON_CLICK", entity };
+    el.onclick = () => { this.store.dispatch(action); };
   }
   
   execute() {
-    this.getEntities().map(this.render)
+    this.getEntities().map(this.render);
   }
 }
 
 /** MAIN **/
-const playerEnt = createEntity(store);
-addComponent(playerEnt, RenderComponent(), store);
-addComponent(playerEnt, PositionComponent(), store);
-
-const locationEnt = createEntity(store);
-addComponent(locationEnt, RenderComponent(), store);
-addComponent(locationEnt, PositionComponent(), store);
+const store = new Store();
 
 const render_system_params = {
   store,
   componentDeps: ["Render", "Position"]
 };
-
+const click_system_params = {
+  store,
+  componentDeps: ["Click"]
+};
 const systems = [
+  new ClickSystem(click_system_params),  
   new RenderSystem(render_system_params)
 ];
+
+const getPositionByIndex = (idx) => {
+  switch (idx) {
+    case 0: return "NW";
+    case 1: return "N";
+    case 2: return "NE";
+    case 3: return "W";
+    case 4: return "C";
+    case 5: return "E";
+    case 6: return "SW";
+    case 7: return "S";
+    case 8: return "SE";
+    default:
+      log("Idx doesn't map to any position");
+      return false;
+  }
+}
+
+const onClick = (entity) => {
+  // do something
+}
+const createCell = (idx) => {
+  const entity = createEntity(store);
+  addComponent(entity,
+	       PositionComponent({ location: getPositionByIndex(idx) }),
+	       store);
+  addComponent(entity,
+	       ClickComponent({onClick}),
+	       store);
+  addComponent(entity,
+	       RenderComponent({model: "xxx"}),
+	       store);
+  return entity;
+};
+const cells = times(9, (_, i) => createCell(i));
+const [head, ...rest] = cells;
+store.dispatch({ type: "CHANGE_LOCATION", entity: head.id, location: "N" });
 
 systems.map(system => system.execute());
